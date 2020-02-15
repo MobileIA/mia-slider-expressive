fb327f228184cbdf95afd5968e109e180033dc03
Matias Camiletti <matias.camiletti@gmail.com>
2020-02-15 05:23:29 -0300

    Agregado servicio para obtener los sliders.

---
+++ src/Handler/ListHandler.php
@@ -0,0 +1,17 @@
+<?php namespace Mobileia\Slider\Handler;
+
+/**
+ * Description of SubscribeHandler
+ *
+ * @author matiascamiletti
+ */
+class ListHandler extends \Mobileia\Expressive\Auth\Request\MiaAuthRequestHandler
+{
+    public function handle(\Psr\Http\Message\ServerRequestInterface $request): \Psr\Http\Message\ResponseInterface
+    {
+        // Obtener todos los sliders activos
+        $rows = \Mobileia\Slider\Model\Slider::where('status', \Mobileia\Slider\Model\Slider::STATUS_ACTIVE)->get();
+        // Devolvemos datos del usuario
+        return new \Mobileia\Expressive\Diactoros\MiaJsonResponse($rows->toArray());
+    }
+}
