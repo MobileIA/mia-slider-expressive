f3527b453f698a414c755929838315dc3feaa4c1
Matias Camiletti <matias.camiletti@gmail.com>
2020-03-30 11:06:50 -0300

    Ocultar eliminados.

--- src/Handler/ListHandler.php
+++ src/Handler/ListHandler.php
@@ -10,7 +10,10 @@ class ListHandler extends \Mobileia\Expressive\Auth\Request\MiaAuthRequestHandle
     public function handle(\Psr\Http\Message\ServerRequestInterface $request): \Psr\Http\Message\ResponseInterface
     {
         // Obtener todos los sliders activos
-        $rows = \Mobileia\Slider\Model\Slider::where('status', \Mobileia\Slider\Model\Slider::STATUS_ACTIVE)->get();
+        $rows = \Mobileia\Slider\Model\Slider::
+                where('status', \Mobileia\Slider\Model\Slider::STATUS_ACTIVE)
+                ->where('deleted', 0)
+                ->get();
         // Devolvemos datos del usuario
         return new \Mobileia\Expressive\Diactoros\MiaJsonResponse($rows->toArray());
     }
